d8a5523fc41f4acff504c0c90c53df54aadf0ee9
corey <corey.cushnie@gmail.com>
2024-06-23 15:23:27 -0400

    layout

--- apps/overall/app/layout.tsx
+++ apps/overall/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="container">{children}</body>
     </html>
   );
 }
