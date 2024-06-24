5234864ae6cea54be2117f769deb951fe733f83b
corey <corey.cushnie@gmail.com>
2024-06-24 02:13:18 -0400

    button

--- apps/overall/app/components/common/Button/Button.tsx
+++ apps/overall/app/components/common/Button/Button.tsx
@@ -6,7 +6,7 @@ export function Button({ name, gradient, onClick }: Button) {
               font-medium rounded 
               px-3 py-2 w-fit 
               hover:bg-zinc-700 cursor-pointer
-              ${gradient && 'gradient-border'}
+              ${gradient && 'gradient-border hover:gradient-border'}
 
               `}
     >
