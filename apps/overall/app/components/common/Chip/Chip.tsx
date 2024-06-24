b5cf4bb0c339afbf02216796c46a2a6a44828458
corey <corey.cushnie@gmail.com>
2024-06-23 20:54:57 -0400

    move chip

---
+++ apps/overall/app/components/common/Chip/Chip.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export function Chip({ name, active, status }: Chip) {
+  const [color, setColor] = useState('');
+  const colors: { [key: string]: string } = {
+    pending: 'text-yellow-500 bg-yellow-100',
+    approved: 'text-green-500 bg-green-100',
+    denied: 'text-red-500 bg-red-100',
+  };
+
+  useEffect(() => {
+    setColor(colors[status]);
+  }, []);
+
+  return (
+    <div
+      className={`text-xs w-fit py-2 px-3 rounded ${
+        !active && 'opacity-20 grayscale'
+      } ${color}`}
+    >
+      <p className="">{name.toLocaleUpperCase()}</p>
+    </div>
+  );
+}
+
+export default Chip;
