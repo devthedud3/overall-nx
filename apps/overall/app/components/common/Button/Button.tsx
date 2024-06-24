b9ce6b3086bca2903ec57cd52cae49010e77ec21
corey <corey.cushnie@gmail.com>
2024-06-23 20:54:46 -0400

    build button

---
+++ apps/overall/app/components/common/Button/Button.tsx
@@ -0,0 +1,18 @@
+export function Button({ name, gradient, onClick }: Button) {
+  return (
+    <p
+      className={`
+              transition ease-in text-sm 
+              font-medium rounded 
+              px-3 py-2 w-fit 
+              hover:bg-zinc-700 cursor-pointer
+              ${gradient && 'gradient-border'}
+
+              `}
+    >
+      {name}
+    </p>
+  );
+}
+
+export default Button;
