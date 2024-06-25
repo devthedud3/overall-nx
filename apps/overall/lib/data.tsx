402146673866190f9570ee031296faa0631f3a8c
corey <corey.cushnie@gmail.com>
2024-06-24 22:41:22 -0400

    data

--- apps/overall/lib/data.tsx
+++ apps/overall/lib/data.tsx
@@ -1,58 +1,145 @@
 export const socials = [
-  { name: "twitter", link: "/" },
-  { name: "youtube", link: "/" },
-  { name: "discord", link: "/" },
-  { name: "instagram", link: "/" },
+  { name: 'twitter', link: '/' },
+  { name: 'youtube', link: '/' },
+  { name: 'discord', link: '/' },
+  { name: 'instagram', link: '/' },
 ];
 
 export const footer = [
   {
-    name: "Company",
+    name: 'Company',
     tabs: [
-      "Our Story",
-      "How It Works",
-      "Pricing",
-      "Careers",
-      "Newsroom",
-      "Referral Program",
-      "Testimonials",
+      'Our Story',
+      'How It Works',
+      'Pricing',
+      'Careers',
+      'Newsroom',
+      'Referral Program',
+      'Testimonials',
     ],
   },
   {
-    name: "Support",
-    tabs: ["Community", "Contact Us", "Customers", "Feedback", "Join Discord"],
+    name: 'Support',
+    tabs: ['Community', 'Contact Us', 'Customers', 'Feedback', 'Join Discord'],
   },
   {
-    name: "Resources",
-    tabs: ["Blog", "Skills Explorer", "Learning Hub", "FAQs"],
+    name: 'Resources',
+    tabs: ['Blog', 'Skills Explorer', 'Learning Hub', 'FAQs'],
   },
 ];
 
-Services: [
-  "Custom Software Development",
-  "Mobile App Development",
-  "Web Development",
-  "UI/UX Design",
-  "Quality Assurance & Testing",
-  "DevOps & Cloud Services",
+export const PRICING = [
+  {
+    name: 'Basic Plan',
+    monthlyPrice: 49,
+    yearlyPrice: 39,
+    description: 'Best for homeowners and small property management companies.',
+    features: [
+      'Up to 10 Units',
+      'Basic Tenant Manangement',
+      'Online Maintainence Tracking',
+      'Basic Rent Collection',
+      'Basic Reporting',
+      'Email Support',
+      '-',
+      '-',
+      '-',
+    ],
+  },
+  {
+    name: 'Standard Plan',
+    monthlyPrice: 99,
+    yearlyPrice: 79,
+    description: 'Best for homeowners and small property management companies.',
+    features: [
+      'Up to 50 Units',
+      'Advanced Tenant Manangement',
+      'Priorty Maintainence Support',
+      'Automated Late Fees',
+      'Advanced Reporting',
+      'Email and Phone Support',
+      'Accounting Software',
+      '-',
+      '-',
+    ],
+  },
+  {
+    name: 'Premium Plan',
+    monthlyPrice: 199,
+    yearlyPrice: 149,
+    description: 'Best for homeowners and small property management companies.',
+    features: [
+      'Unlimited Units',
+      'Comprehensive Tenant Manangement',
+      '24/7 Priority Support',
+      'Multiple Payment Methods',
+      'Comprehensive Reporting',
+      '24/7 Product Support',
+      'Accounting, CRM, and More',
+      'Automated Marketing',
+      'Secure Storage & Management',
+    ],
+  },
 ];
 
-AboutUs: [
-  "Our Story",
-  "Our Team",
-  "Careers",
-  "Testimonials",
-  "Partnerships",
-  "Contact Us",
+export const FEATURE_TABLE = [
+  {
+    title: 'Features',
+    features: [
+      'Property Listings',
+      'Tenant Management',
+      'Maintenance Requests',
+      'Payment Processing',
+      'Reporting',
+      'Customer Support',
+      'Integrations',
+      'Marketing Tools',
+      'Document Management',
+    ],
+  },
+  {
+    title: 'Basic Plan',
+    price: 9,
+    features: [
+      'Up to 10',
+      'Basic',
+      'Online Tracking',
+      'Basic Rent Collection',
+      'Basic Reporting',
+      'Email Support',
+      '-',
+      '-',
+      '-',
+    ],
+  },
+  {
+    title: 'Standard Plan',
+    price: 9,
+    features: [
+      'Up to 50',
+      'Advanced',
+      'Priorty Support',
+      'Automated Late Fees',
+      'Advanced Reporting',
+      'Email and Phone Support',
+      'Accounting Software',
+      '-',
+      '-',
+    ],
+  },
+  {
+    title: 'Premium Plan',
+    price: 99,
+    features: [
+      'Unlimited',
+      'Comprehensive',
+      '24/7 Priority Support',
+      'Multiple Payment Methods',
+      'Comprehensive Reporting',
+      '24/7 Support',
+      'Accounting, CRM, and More',
+      'Automated Marketing',
+      'Secure Storage & Management',
+    ],
+  },
 ];
-
-Resources: [
-  "Blog",
-  "Case Studies",
-  "Whitepapers & Ebooks",
-  "Webinars & Events",
-  "FAQs",
-  "Support Center",
-];
-
-Connect: ["Subscribe to Newsletter"];
