04e51d854fb0167423ee5f6ec1110e97e33b8496
corey <corey.cushnie@gmail.com>
2024-06-24 22:41:30 -0400

    init icons

--- apps/overall/public/Icons.tsx
+++ apps/overall/public/Icons.tsx
@@ -1,13 +1,17 @@
-import { FaXTwitter } from "react-icons/fa6";
+import { FaXTwitter } from 'react-icons/fa6';
 import {
   PiInstagramLogoDuotone,
   PiYoutubeLogoDuotone,
   PiDiscordLogoFill,
-} from "react-icons/pi";
+  PiCheckCircleFill,
+  PiXCircleFill,
+} from 'react-icons/pi';
 
 export const IC: { [key: string]: any } = {
   twitter: <FaXTwitter size={20} />,
   instagram: <PiInstagramLogoDuotone size={20} />,
   youtube: <PiYoutubeLogoDuotone size={20} />,
   discord: <PiDiscordLogoFill size={20} />,
+  check: <PiCheckCircleFill size={15} color="lime" />,
+  notCheck: <PiXCircleFill size={15} color="pink" />,
 };
