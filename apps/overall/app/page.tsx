ccdaaaa8a5fec0802d059a7967cb6b8618545819
corey <corey.cushnie@gmail.com>
2024-06-24 22:40:34 -0400

    build page

--- apps/overall/app/page.tsx
+++ apps/overall/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button, Chip } from './components/common';
-import { Footer, Header } from './components';
+import { Footer, Header, Pricing } from './components';
 import { FaRegSmileBeam } from 'react-icons/fa';
 import { MdDashboardCustomize } from 'react-icons/md';
 import { LuArrowBigUpDash } from 'react-icons/lu';
 import { PiBrainDuotone } from 'react-icons/pi';
+import Image from 'next/image';
 
 export default function Landing() {
   return (
@@ -12,7 +13,7 @@ export default function Landing() {
       <section className="container flex my-48 items-center">
         <div className="h-full">
           <h1 className="text-6xl w-full font-extrabold pb-6">
-            Turn-key property management solution
+            The only property management solution
           </h1>
           <p className="text-sm pb-16">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
@@ -20,15 +21,26 @@ export default function Landing() {
             est enim earum! Id odit deleniti perferendis architecto est rem
             atque?
           </p>
-          <Chip name="25% off for the first 3 months" status="denied" active />
+          <Chip
+            name="25% off for the first 3 months"
+            status="approved"
+            active
+          />
           <div className="flex space-x-2 pt-8">
             {['Request a demo', 'Learn More'].map((item, index) => (
               <Button key={index} name={item} gradient={index === 0} />
             ))}
           </div>
         </div>
-        <div className="border flex h-96 items-center justify-center w-full">
-          <p>image goes here</p>
+        <div className="flex h-96 items-center justify-center w-full">
+          {/* <p>image goes here</p> */}
+          <Image
+            src="/login.png"
+            height={1000}
+            width={1000}
+            alt=""
+            style={{ width: 'auto' }}
+          />
         </div>
       </section>
       <section className="py-12 border-t border-b border-zinc-800 w-full h-fit flex justify-center">
@@ -54,8 +66,8 @@ export default function Landing() {
           )}
         </div>
       </section>
-      <section className="container w-full py-24 w-[1140px]">
-        <div className="text-sm border">testing</div>
+      <section className="container w-full py-24">
+        <Pricing />
       </section>
       <Footer />
     </div>
